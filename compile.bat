ac4cf9c2f2ccea0e0301a5f84472298f3d81e624
Castano-ALFA <Castano@inbox.com>
2014-08-30 16:52:02 -0400

    This is for MS, not BG! Also remove included ncs/ndb files.

--- compile.bat
+++ compile.bat
@@ -1,3 +1,4 @@
 del *.ncs
 del *.ndb
-NWNScriptCompiler.exe -e -v1.70 -o -h "C:\Users\ALFA-MS\Documents\Neverwinter Nights 2" -m alfa010_bg .\*.nss -y
+NWNScriptCompiler.exe -e -v1.70 -o -h "C:\Users\ALFA-MS\Documents\Neverwinter Nights 2" -m ALFA-009-Moonshaes .\*.nss -y
+pause
