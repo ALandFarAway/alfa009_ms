db9ee015a4f90689c8adfb1d8e3910ce60151cb7
shad0wfax-alfa <tindari_rathe@yahoo.com>
2018-04-08 20:07:22 -0700

    changing compile.bat

--- compile.bat
+++ compile.bat
@@ -1,4 +1,3 @@
 del *.ncs
 del *.ndb
-NWNScriptCompiler.exe -e -v1.70 -j -o -h "C:\Users\ALFA-009\Documents\Neverwinter Nights 2" -m ALFA-009-Moonshaes .\*.nss -y > C:\Users\ALFA-009\Desktop\out.txt
-pause
+NWNScriptCompiler.exe -e -v1.70 -j -o -h "C:\Users\ALFA-009\Documents\Neverwinter Nights 2" -m ALFA-009-Moonshaes .\*.nss -y
