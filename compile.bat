a3196df4dc114da4db917dc5efd8ee99da9a9bfa
shad0wfax-alfa <tindari_rathe@yahoo.com>
2018-04-08 20:02:47 -0700

    module.IFO , moduledownloaderresources.xml , and compile.bat update for MS to support rollout of ACR.

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 del *.ncs
 del *.ndb
-NWNScriptCompiler.exe -e -v1.70 -o -h "C:\Users\ALFA-009\Documents\Neverwinter Nights 2" -m ALFA-009-Moonshaes .\*.nss -y
+NWNScriptCompiler.exe -e -v1.70 -j -o -h "C:\Users\ALFA-009\Documents\Neverwinter Nights 2" -m ALFA-009-Moonshaes .\*.nss -y > C:\Users\ALFA-009\Desktop\out.txt
 pause
